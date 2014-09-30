7fac41757f4487f82c05bec4ab3b8b96a825b77f
gregerg <gregerg@gmail.com>
2014-09-30 10:43:04 -0700

    posting, getting, and optionsing

--- server/request-handler.js
+++ server/request-handler.js
@@ -20,17 +20,44 @@ module.exports = function (request, response) {
    * below about CORS. */
   var headers = defaultCorsHeaders;
 
-  headers['Content-Type'] = "text/plain";
+  // headers['Content-Type'] = "text/plain";
+  headers['Content-Type'] = "application/json";
+
+  switch(request.method) {
+    case "GET":
+      response.writeHead(statusCode, headers);
+      response.write(JSON.stringify(chatMessages));
+      break;
+    case "POST":
+      statusCode = 201;
+      var incoming = "";
+      request.on('data', function(data) {
+        incoming += data;
+        console.log(incoming);
+      });
+      request.on('end', function() {
+        var inJSON = JSON.parse(incoming);
+        chatMessages.results.push(inJSON);
+      });
+      response.write(JSON.stringify(chatMessages));
+      break;
+    case "OPTIONS":
+      response.writeHead(statusCode, headers);
+      break;
+  }
 
   /* .writeHead() tells our server what HTTP status code to send back */
-  response.writeHead(statusCode, headers);
 
+  // response.writeHead(statusCode, headers);
   /* Make sure to always call response.end() - Node will not send
    * anything back to the client until you do. The string you pass to
    * response.end() will be the body of the response - i.e. what shows
    * up in the browser.*/
-  response.end("Hello, Gerg!");
+  response.end();
 };
+
+var chatMessages = {};
+chatMessages.results = [{objectId : 1, createdAt: "10", roomname: "lobby", username: "rishigoooomar", text: "GOOOOOOOOOMAR"}];
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
  * This CRUCIAL code allows this server to talk to websites that
